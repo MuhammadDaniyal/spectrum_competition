7eeed4e309d6ae2abde2af24830f534a4c14b617
Obaid <obaidurrehman8121@gmail.com>
2023-06-22 13:43:01 +0500

    task done

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Main from '../Main/Main'
 import Layout from '../../Layout'
+import Mission from "../Mission/Mission"
+import Footer from '../Footer/Footer'
 const Home = () => {
     return (
         <>
             <Layout>
                 <Main />
+                <Mission></Mission>
+                <Footer></Footer>
             </Layout>
         </>
     )
